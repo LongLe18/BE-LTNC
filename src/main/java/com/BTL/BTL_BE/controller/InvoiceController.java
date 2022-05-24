c38a05f3722333badd7dea63f328dbcfa357b81d
0963403400 <nguyenhuytuan291100@gmail.com>
2022-05-24 08:22:05 +0700

    Tuan

--- src/main/java/com/BTL/BTL_BE/controller/InvoiceController.java
+++ src/main/java/com/BTL/BTL_BE/controller/InvoiceController.java
@@ -7,19 +7,23 @@ package com.BTL.BTL_BE.controller;
 import com.BTL.BTL_BE.Dao.InvoiceDAO;
 import com.BTL.BTL_BE.Dao.SeasonDAO;
 import com.BTL.BTL_BE.entity.Invoice;
+import com.BTL.BTL_BE.entity.Product;
 import com.BTL.BTL_BE.entity.Season;
 import com.BTL.BTL_BE.payload.response.MessageResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Date;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -98,7 +102,7 @@ public class InvoiceController {
 //            java.time.LocalDate.now(),
                     new Date(today.getTime()),
                     invoice.getStatusOrder(),invoice.getTotalInvoice(),invoice.getIdShipper(),invoice.getIdEmployee(),
-                    new java.util.Date(today.getTime()+(1000 * 60 * 60 * 24)));
+                    new java.util.Date(today.getTime()+(1000 * 60 * 60 * 24*3)));
 //                    java.time.LocalDate.now().plusDays(3));
             invoicedao.save(InvoiceInsert);
             result.setData(InvoiceInsert);
@@ -111,56 +115,56 @@ public class InvoiceController {
             return new ResponseEntity<MessageResponse>(result, HttpStatus.BAD_REQUEST);
         }    
     }  
-//    @PutMapping("/ChangeProduct")
-//    public ResponseEntity<MessageResponse> ChangeProduct(@RequestParam(name="id") String ID,
-//  @Valid @RequestBody Product product)
-//    {
-//        MessageResponse result = new MessageResponse();
-//        try {
-//            Product ExistProduct=productdao.findByIDProduct(ID).get(0);
-////            Integer id= (Integer.parseInt(ExistProduct.getID_Product())+1);
-////            Product ProductInsert=new Product(id.toString(),product.getName_Product(),product.getImage(),product.getQuantity(),
-////                    product.getDescribe(),product.getPrice(),product.getSale(),product.getWarranty_Period(),
-////                    product.getID_Category(),product.getID_Brand(),product.getID_Season());
-//            ExistProduct.setDescribe(product.getDescribe());
-//            ExistProduct.setID_Brand(product.getID_Brand());
-//            ExistProduct.setID_Season(product.getID_Season());
-//            ExistProduct.setImage(product.getImage());
-//            ExistProduct.setName_Product(product.getName_Product());
-//            ExistProduct.setPrice(product.getPrice());
-//            ExistProduct.setQuantity(product.getQuantity());
-//            ExistProduct.setSale(product.getSale());
-//            ExistProduct.setWarranty_Period(product.getWarranty_Period());
-//            ExistProduct.setID_Category(product.getID_Category());
-//            productdao.save(ExistProduct);
-//            result.setData(ExistProduct);
-//            result.setMessage("Thay đổi sản phẩm thành công");
-//            return new ResponseEntity<MessageResponse>(result, HttpStatus.OK);
-//        } catch(Exception e) {
-//            result.setStatus(MessageResponse.Status.FAILED);
-//            result.setMessage("Lỗi " + e);
-//                
-//            return new ResponseEntity<MessageResponse>(result, HttpStatus.BAD_REQUEST);
-//        }    
-//    }  
-//    @DeleteMapping("/Delete")
-//    public ResponseEntity<MessageResponse> DeleteProduct(@RequestParam(name="id") String ID)
-//    {
-//        MessageResponse result = new MessageResponse();
-//        try {
-//            Product ExistProduct=productdao.findByIDProduct(ID).get(0);
-////         
-//            productdao.delete(ExistProduct);
-////            result.setData(ExistProduct);
-//            result.setMessage("Xóa sản phẩm thành công");
-//            return new ResponseEntity<MessageResponse>(result, HttpStatus.OK);
-//        } catch(Exception e) {
-//            result.setStatus(MessageResponse.Status.FAILED);
-//            result.setMessage("Lỗi " + e);
-//                
-//            return new ResponseEntity<MessageResponse>(result, HttpStatus.BAD_REQUEST);
-//        }    
-//    }  
+    @PutMapping("/ChangeInvoice")
+    public ResponseEntity<MessageResponse> ChangeInvoice(@RequestParam(name="id") String ID,
+  @Valid @RequestBody Invoice invoice)
+    {
+        MessageResponse result = new MessageResponse();
+        try {
+            Invoice ExistInvoice=invoicedao.findByIdInvoice(ID);
+//            Integer id= (Integer.parseInt(ExistProduct.getID_Product())+1);
+//            Product ProductInsert=new Product(id.toString(),product.getName_Product(),product.getImage(),product.getQuantity(),
+//                    product.getDescribe(),product.getPrice(),product.getSale(),product.getWarranty_Period(),
+//                    product.getID_Category(),product.getID_Brand(),product.getID_Season());
+            ExistInvoice.setIdAccount(invoice.getIdAccount());
+            ExistInvoice.setShippingAddress(invoice.getShippingAddress());
+            ExistInvoice.setPaymentMethods(invoice.getPaymentMethods());
+            ExistInvoice.setStatusOrder(invoice.getStatusOrder());
+            ExistInvoice.setTotalInvoice(invoice.getTotalInvoice());
+            ExistInvoice.setIdShipper(invoice.getIdShipper());
+            ExistInvoice.setIdEmployee(invoice.getIdEmployee());
+            ExistInvoice.setPurchaseDate(invoice.getPurchaseDate());
+            ExistInvoice.setTimeLimit(invoice.getTimeLitmit());
+            
+            invoicedao.save(ExistInvoice);
+            result.setData(ExistInvoice);
+            result.setMessage("Thay đổi hóa đơn thành công");
+            return new ResponseEntity<MessageResponse>(result, HttpStatus.OK);
+        } catch(Exception e) {
+            result.setStatus(MessageResponse.Status.FAILED);
+            result.setMessage("Lỗi " + e);
+                
+            return new ResponseEntity<MessageResponse>(result, HttpStatus.BAD_REQUEST);
+        }    
+    }  
+    @DeleteMapping("/Delete")
+    public ResponseEntity<MessageResponse> DeleteInvoice(@RequestParam(name="id") String ID)
+    {
+        MessageResponse result = new MessageResponse();
+        try {
+            Invoice ExistInvoice=invoicedao.findByIdInvoice(ID);
+//         
+            invoicedao.delete(ExistInvoice);
+            result.setData(ExistInvoice);
+            result.setMessage("Xóa hóa đơn thành công");
+            return new ResponseEntity<MessageResponse>(result, HttpStatus.OK);
+        } catch(Exception e) {
+            result.setStatus(MessageResponse.Status.FAILED);
+            result.setMessage("Lỗi " + e);
+                
+            return new ResponseEntity<MessageResponse>(result, HttpStatus.BAD_REQUEST);
+        }    
+    }  
     
     
 }
